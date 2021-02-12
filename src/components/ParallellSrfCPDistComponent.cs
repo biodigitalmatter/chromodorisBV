b445ea3a63300b4ec5fe853512696966b3e7e12c
Anton Tetov <anton@tetov.se>
2021-02-12 11:30:33 +0100

    Move methods to component

--- src/components/ParallellSrfCPDistComponent.cs
+++ src/components/ParallellSrfCPDistComponent.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 using Grasshopper.Kernel;
-using Grasshopper.Kernel.Data;
-using Grasshopper.Kernel.Types;
+
 using Rhino.Geometry;
 
 namespace Chromodoris
@@ -24,14 +26,16 @@ namespace Chromodoris
         /// Initializes a new instance of the AverageDistancesToPointclouds class.
         /// </summary>
         public ParallellSrfCPDistComponent()
-          : base("Parallell Srf Closest Point Distance", "ParSrfCPDist",
-                 "Find the distance to the closest point on surface, in parallell", "ChromodorisBV", "Extra")
+          : base(
+                "Parallell Srf Closest Point Distance",
+                "ParSrfCPDist",
+                "Find the distance to the closest point on surface, in parallell",
+                "ChromodorisBV",
+                "Extra")
         {
         }
 
         #endregion Constructors
-
-
 
         #region Properties
 
@@ -39,32 +43,36 @@ namespace Chromodoris
         /// Gets the unique ID for this component. Do not change this ID after release.
         /// </summary>
         public override Guid ComponentGuid => new Guid("35EF6DCE-F540-40EC-99BB-6C32A75BCE89");
+
+        // Test if this speeds up component
+        public override bool IsPreviewCapable => false;
+
         /// <summary>
         /// Provides an Icon for the component.
         /// </summary>
-        protected override System.Drawing.Bitmap Icon
-        {
-            get
-            {
-                //You can add image files to your project resources and access them like this:
-                // return Resources.IconForThisComponent;
-                return null;
-            }
-        }
+        //You can add image files to your project resources and access them like this:
+        // return Resources.IconForThisComponent;
+        protected override System.Drawing.Bitmap Icon => null;
 
         #endregion Properties
 
-
-
         #region Methods
-
         /// <summary>
         /// Registers all the input parameters for this component.
         /// </summary>
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
-            _inSamplePtsIdx = pManager.AddPointParameter("Points", "P", "Sample point", GH_ParamAccess.list);
-            _inSrfIdx = pManager.AddSurfaceParameter("Surface", "S", "Base surface.", GH_ParamAccess.item);
+            _inSamplePtsIdx = pManager.AddPointParameter(
+                "Points",
+                "P",
+                "Sample point",
+                GH_ParamAccess.list);
+
+            _inSrfIdx = pManager.AddSurfaceParameter(
+                "Surface",
+                "S",
+                "Base surface.",
+                GH_ParamAccess.item);
         }
 
         /// <summary>
@@ -72,7 +80,11 @@ namespace Chromodoris
         /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
-            _outDistIdx = pManager.AddNumberParameter("Distance", "D", "Distance between sample point and closest point on surface.", GH_ParamAccess.list);
+            _outDistIdx = pManager.AddNumberParameter(
+                "Distance",
+                "D",
+                "Distance between sample point and closest point on surface.",
+                GH_ParamAccess.list);
         }
 
         /// <summary>
@@ -85,19 +97,41 @@ namespace Chromodoris
             Surface srf = null;
 
             if (!DA.GetDataList(_inSamplePtsIdx, pts))
-            {
                 return;
-            }
+
+            var pts_array = pts.ToArray();
 
             if (!DA.GetData(_inSrfIdx, ref srf))
-            {
                 return;
-            }
 
+            _ = DA.SetDataList(_outDistIdx, GetDistances(pts_array, srf));
+        }
 
-            var parSrfDistCls = new ParallellSrfCPDist(pts, srf, false);
+        internal List<double> GetDistances(Point3d[] pts, Surface srf)
+        {
+            var dists = new double[pts.Length];
 
-            DA.SetDataList(_outDistIdx, parSrfDistCls.ComputeMultiThreaded());
+            // Load balance = True, based on the assumption that this operation's
+            // execution time varies.
+            // var partitioner = Partitioner.Create(Enumerable.Range(0, pts.Length).ToList(), true);
+
+            // Larger ranges to use be able top update thread specific u1 and v1.
+            var partitioner = Partitioner.Create(0, pts.Length);
+
+            _ = Parallel.ForEach(partitioner, (range, loopstate) =>
+            {
+                for (int i = range.Item1; i < range.Item2; i++)
+                {
+                    if (!srf.ClosestPoint(pts[i], out double u, out double v))
+                    {
+                        AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Closest point not found.");
+                        loopstate.Break();
+                    }
+                    dists[i] = pts[i].DistanceTo(srf.PointAt(u, v));
+                }
+            });
+
+            return dists.ToList();
         }
 
         #endregion Methods
