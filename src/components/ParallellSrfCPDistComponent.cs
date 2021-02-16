afacdf2d642dcb414595193cc80ba8c8a0bb9602
Anton Tetov <anton@tetov.se>
2021-02-16 15:08:18 +0100

    Testing showed this to be way slower, memory consumption probably.

--- src/components/ParallellSrfCPDistComponent.cs
+++ src/components/ParallellSrfCPDistComponent.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,7 +9,7 @@ using Rhino.Geometry;
 
 namespace Chromodoris
 {
-    public class ParallellSrfCPDistComponent : GH_Component
+    public class ParallellSrfCPDistComponent : GH_TaskCapableComponent<ParallellSrfCPDistComponent.SolveResult>
     {
         private int _inSamplePtsIdx;
         private int _inSrfIdx;
@@ -53,7 +52,7 @@ namespace Chromodoris
                 "Points",
                 "P",
                 "Sample point",
-                GH_ParamAccess.list);
+                GH_ParamAccess.item);
 
             _inSrfIdx = pManager.AddSurfaceParameter(
                 "Surface",
@@ -71,8 +70,10 @@ namespace Chromodoris
                 "Distance",
                 "D",
                 "Distance between sample point and closest point on surface.",
-                GH_ParamAccess.list);
+                GH_ParamAccess.item);
         }
+
+        private delegate void Del();
 
         /// <summary>
         /// This is the method that actually does the work.
@@ -80,64 +81,115 @@ namespace Chromodoris
         /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
         protected override void SolveInstance(IGH_DataAccess DA)
         {
-            Surface srf = null;
-            var pts = new List<Point3d>();
+            Del errorNoPtsDelegate = () => AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "No closest point found.");
 
-            if (!DA.GetDataList(_inSamplePtsIdx, pts))
+            if (InPreSolve)
             {
+                // First pass; collect data and construct tasks
+
+                SolveData solveData = GetInputs(DA);
+
+                Task<SolveResult> tsk = null;
+                {
+                    tsk = Task.Run(() => ComputeDistance(solveData, errorNoPtsDelegate), CancelToken);
+                }
+                TaskList.Add(tsk);
+
                 return;
             }
 
+            if (!GetSolveResults(DA, out SolveResult results))
+            {
+                // Compute right here, right now.
+                // 1. Collect
+                SolveData solveData = GetInputs(DA);
+
+                // 2. Compute
+                results = ComputeDistance(solveData, errorNoPtsDelegate);
+            }
+
+            // 3. Set
+            if (results != null)
+            {
+                _ = DA.SetData(_outDistIdx, results.Value);
+            }
+        }
+
+        private SolveData GetInputs(IGH_DataAccess DA)
+        {
+            Point3d pt = new Point3d();
+            Surface srf = null;
+            if (!DA.GetData(_inSamplePtsIdx, ref pt))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Input P failed to collect data.");
+            }
             if (!DA.GetData(_inSrfIdx, ref srf))
             {
-                return;
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Input S failed to collect data.");
             }
 
-            SolveData solveData = new SolveData(pts, srf);
-
-            _ = DA.SetDataList(_outDistIdx, GetDistances(solveData));
+            return new SolveData(pt, srf);
         }
 
-        private List<double> GetDistances(SolveData solveData)
+        //private List<double> GetDistances(SolveData solveData)
+        //{
+        //    var dists = new double[solveData.Pts.Count];
+
+        //    // Load balance = True, based on the assumption that this operation's
+        //    // execution time varies.
+        //    // var partitioner = Partitioner.Create(Enumerable.Range(0, pts.Length).ToList(), true);
+
+        //    // Larger ranges to use be able top update thread specific u1 and v1.
+        //    var partitioner = Partitioner.Create(0, solveData.Pts.Count);
+
+        //    _ = Parallel.ForEach(partitioner, (range, loopstate) => ComputeRange(dists, solveData, range, loopstate));
+
+        //    return dists.ToList();
+        //}
+
+        //private static void ComputeRange(in double[] resultList, SolveData solveData, Tuple<int, int> range, ParallelLoopState loopstate)
+        //{
+        //    for (int i = range.Item1; i < range.Item2; i++)
+        //    {
+        //        if (!solveData.Srf.ClosestPoint(solveData.Pts[i], out double u, out double v))
+        //        {
+        //            // AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Closest point not found.");
+        //            loopstate.Break();
+        //        }
+        //        resultList[i] = solveData.Pts[i].DistanceTo(solveData.Srf.PointAt(u, v));
+        //    }
+        //    // GC.Collect(2, GCCollectionMode.Optimized);
+        //}
+
+        //protected override void AfterSolveInstance() => GC.Collect();
+        private static SolveResult ComputeDistance(SolveData solveData, in Del errorNoPtsDelegate)
         {
-            var dists = new double[solveData.Pts.Count];
-
-            // Load balance = True, based on the assumption that this operation's
-            // execution time varies.
-            // var partitioner = Partitioner.Create(Enumerable.Range(0, pts.Length).ToList(), true);
-
-            // Larger ranges to use be able top update thread specific u1 and v1.
-            var partitioner = Partitioner.Create(0, solveData.Pts.Count);
-
-            _ = Parallel.ForEach(partitioner, (range, loopstate) => ComputeRange(dists, solveData, range, loopstate));
-
-            return dists.ToList();
-        }
-
-        private static void ComputeRange(in double[] resultList, SolveData solveData, Tuple<int, int> range, ParallelLoopState loopstate)
-        {
-            for (int i = range.Item1; i < range.Item2; i++)
+            if (solveData.Srf.ClosestPoint(solveData.Pt, out double u, out double v))
             {
-                if (!solveData.Srf.ClosestPoint(solveData.Pts[i], out double u, out double v))
-                {
-                    // AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Closest point not found.");
-                    loopstate.Break();
-                }
-                resultList[i] = solveData.Pts[i].DistanceTo(solveData.Srf.PointAt(u, v));
+                return new SolveResult(solveData.Pt.DistanceTo(solveData.Srf.PointAt(u, v)));
             }
-            // GC.Collect(2, GCCollectionMode.Optimized);
+
+            // Raise if no pt found
+            errorNoPtsDelegate();
+            return null;
         }
 
-        protected override void AfterSolveInstance() => GC.Collect();
-
-        public readonly struct SolveData
+        private readonly struct SolveData
         {
-            public List<Point3d> Pts { get; }
+            public Point3d Pt { get; }
             public Surface Srf { get; }
-            public SolveData(in List<Point3d> pts, in Surface srf)
+            public SolveData(Point3d pt, in Surface srf)
             {
-                Pts = pts;
+                Pt = pt;
                 Srf = srf;
+            }
+        }
+        public class SolveResult
+        {
+            public double Value { get; }
+            public SolveResult(double value)
+            {
+                Value = value;
             }
         }
     }
